d099f7b2e5ab31abf81bb8fa555c78ea523ed77f
Agustín Pascual <juan_agustin09@hotmail.com>
2022-10-12 19:36:19 -0300

    Clase Django - Porftolio (parte 1)

---
+++ proyectoClase/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.template import Context, Template
+
+def hola(request):
+    return HttpResponse ("<h1>Buenas clase 41765!</h1>")
+
+def fecha(request):
+    fecha_y_hora = datetime.now()
+    return HttpResponse(f"La fecha y hora actual es {fecha_y_hora}")
+
+def calcular_fecha_nacimiento(request, edad):
+    fecha = datetime.now().year - edad
+    return HttpResponse(f"Tu fecha de nacimiento aproximada para tu {edad} años sería: {fecha}")
+
+def mi_template(request):
+    
+    cargar_archivo = open(r"C:\Users\Admin\OneDrive\Python - Django\proyecto\templates\template.html", "r")
+    template = Template(cargar_archivo.read())
+    cargar_archivo.close()
+    
+    contexto = Context()
+    
+    template_renderizado = template.render(contexto)
+     
+    return HttpResponse(template_renderizado)
